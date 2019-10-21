a7bb8496554a6f1f79037bf8e9ee238d85a9616d
L0stCoder <45640225+L0stCoder@users.noreply.github.com>
2019-10-21 15:53:41 -0500

    Update and rename V2.java to SemesterTest.java

--- SemesterTest.java
+++ SemesterTest.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class V2{
+public class SemesterTest{
 	public static void main(String[]args){
 		Scanner input=new Scanner(System.in);
 		//Item randomization on start
